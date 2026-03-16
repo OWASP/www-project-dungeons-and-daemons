4a707394041d90c95c175518c3f1241291d16e92
Johan Sydseter <johan.sydseter@admincontrol.com>
2026-03-16 10:04:30 +0100

    fix unsafe-inline

--- script/headers.js
+++ script/headers.js
@@ -25,7 +25,7 @@ function main() {
   Referrer-Policy: same-origin
   Permissions-Policy: accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), fullscreen=(self "https://www.youtube.com/"), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(self "https://www.youtube.com/"), publickey-credentials-get=(), sync-xhr=(), usb=(), xr-spatial-tracking=(), geolocation=()
   Strict-Transport-Security: max-age=31536000; includeSubDomains; preload
-  Content-Security-Policy: base-uri 'self'; default-src 'none'; frame-src 'self' www.youtube.com youtube.com; connect-src 'self'; img-src 'self' i.ytimg.com; script-src 'self' 'nonce-DhcnhD3khTMePgXw'; style-src 'self' 'unsafe-inline'; style-src-elem 'self'; upgrade-insecure-requests
+  Content-Security-Policy: base-uri 'self'; default-src 'none'; frame-src 'self' www.youtube.com youtube.com; connect-src 'self'; img-src 'self' 'unsafe-inline' i.ytimg.com; script-src 'self' 'nonce-DhcnhD3khTMePgXw'; style-src 'self' 'unsafe-inline'; style-src-elem 'self'; upgrade-insecure-requests
 /404
   X-Robots-Tag: noindex
 `;
